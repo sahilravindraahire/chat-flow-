e4f451c04e1fa3c55569bc820d3b720dfad011c4
sahil ahire <sahilahire2003@gmail.com>
2026-05-16 14:33:08 +0530

    some minor changes

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -17,6 +17,7 @@ const generateToken = (id) => {
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   maxAge: 8 * 24 * 60 * 60 * 1000,
 };
 
--- server/src/server.js
+++ server/src/server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({extended: true}))
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true
 }))
 
